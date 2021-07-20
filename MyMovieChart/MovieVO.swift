34578144bf3847bca7544a7ff02d6cd3ee5e05e2
hanbee <sonhanbi1002@gmail.com>
2021-07-20 17:03:43 +0900

    영화 정보를 담을 VO 객체 생성

---
+++ MyMovieChart/MovieVO.swift
@@ -0,0 +1,17 @@
+//
+//  MovieVO.swift
+//  MyMovieChart
+//
+//  Created by 손한비 on 2021/07/20.
+//
+
+import Foundation
+
+class MovieVO {
+    var thumbnail: String? // 영화 썸네일 이미지 주소
+    var title: String? // 영화 제목
+    var description: String? // 영화 설명
+    var detail: String? // 상세정보
+    var opendate: String? // 개봉일
+    var rating: Double? // 평점
+}
