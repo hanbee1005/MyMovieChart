312f0606e1f98653894b9870a9959873c2bc9796
hanbee <sonhanbi1002@gmail.com>
2021-07-20 17:22:42 +0900

    초기 테이블 데이터 구성하기

--- MyMovieChart/ListViewController.swift
+++ MyMovieChart/ListViewController.swift
@@ -8,5 +8,33 @@
 import UIKit
 
 class ListViewController: UITableViewController {
+    // 튜플 아이템으로 구성된 데이터 세트
+    var dataset = [
+        ("다크나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95),
+        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31),
+        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19)
+    ]
+    
+    // 테이블 뷰를 구성할 리스트 데이터
+    // lazy 키워드를 사용하면 참조되는 시점에 맞추어 초기화되므로 메모리 낭비를 줄일 수 있고
+    // 다른 프로퍼티(여기서는 dataset)을 참조할 수 있습니다.
+    lazy var list: [MovieVO] = {
+        var datalist = [MovieVO]()
+        for (title, desc, opendate, rating) in self.dataset {
+            let mvo = MovieVO()
+            mvo.title = title
+            mvo.description = desc
+            mvo.opendate = opendate
+            mvo.rating = rating
+            
+            datalist.append(mvo)
+        }
+        
+        return datalist
+    }()
+    
+    override func viewDidLoad() {
+        
+    }
     
 }
