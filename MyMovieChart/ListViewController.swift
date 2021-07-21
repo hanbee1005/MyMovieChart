f59f07919918ab5351bcc0cee7290c2a1e3ef029
hanbee <sonhanbi1002@gmail.com>
2021-07-21 17:30:47 +0900

    전체 데이터를 모두 읽은 경우 더보기 버튼 없애기

--- MyMovieChart/ListViewController.swift
+++ MyMovieChart/ListViewController.swift
@@ -15,6 +15,8 @@ class ListViewController: UITableViewController {
     // 테이블 뷰를 구성할 리스트 데이터
     var list = [MovieVO]()
     
+    @IBOutlet var moreBtn: UIButton!
+    
     @IBAction func more(_ sender: Any) {
         // 현재 페이지 값에 1을 추가
         self.page += 1
@@ -70,6 +72,14 @@ class ListViewController: UITableViewController {
                 
                 // list 배열에 추가
                 self.list.append(mvo)
+            }
+            
+            // 전체 데이터 카운트를 얻는다.
+            let totalCount = (hoppin["totalCount"] as? NSString)!.intValue
+            
+            // totalCount가 읽어온 데이터 크기와 같거나 클 경우 더보기 버튼을 막는다.
+            if (self.list.count >= totalCount) {
+                self.moreBtn.isHidden = true
             }
         } catch {
             NSLog("Parse Error!!!")
